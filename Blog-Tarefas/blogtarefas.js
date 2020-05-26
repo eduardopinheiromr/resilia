31b2a6bbf89ce56d865286a0ace4f38c6c8cbbc0
Eduardo Pinheiro <ddvvfftt@gmail.com>
2020-05-25 23:46:07 -0300

    Projeto criado e atualizado até o momento.

---
+++ Blog-Tarefas/blogtarefas.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', start);
+
+const slidebar = document.querySelector('.div-aviso');
+const main = document.querySelector('#main');
+
+function start() {
+  slidebar.style.marginTop = '50px';
+  slidebar.style.color = '#f0c52c';
+  main.style.marginTop = '50px';
+}
